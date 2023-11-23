97c85b0fdf2fea6bac51b072b685cbc692c924e1
caiopessoa145@gmail.com <caiopessoa145@gmail.com>
2023-11-22 22:45:10 -0300

    logica js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,76 @@
-fetch('https://pokeapi.co/api/v2/pokemon/')
-.then((res)=>{
-    console.log(res);
-    if (res.ok) { 
-        return res.json();
-    }else{
-        console.log("Error");
+
+const nomePokemon           = document.querySelector('.nome_pokemon');
+const numeroPokemon         = document.querySelector('.id_pokemon');
+const imagemPokemon         = document.querySelector('.img_pokemon');
+const formProcurarPokemon   = document.querySelector('.form_procurarPokemon');
+const inputProcurarPokemon  = document.querySelector('.input_buscaPokemon');
+const btnPrevPokemon        = document.querySelector('.btn-prev');
+const bntNextPokemon        = document.querySelector('.btn-next');
+const types = document.querySelector('.types');
+
+let pokemonInicial= 1 ;
+const BuscarPokemon = async (pokemon)=>{
+
+    const respostaAPI= await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    
+        if (respostaAPI.status === 200) {
+            const data = await respostaAPI.json();
+            return data;
     }
- })
- .then((data)=> console.log(data.results))
- .catch((err) => console.log(err))
+
+}
+
+
+const renderPokemon = async (pokemon)=>{
+
+    const data= await BuscarPokemon(pokemon);
+
+    nomePokemon.innerHTML = '...';
+    numeroPokemon.innerHTML = '';
+
+    if (data) {
+
+        nomePokemon.innerHTML=data.name;
+        numeroPokemon.innerHTML=data.id;
+        inputProcurarPokemon.value='';
+        pokemonInicial=data.id;
+
+        if(!data['types'][1]){
+           console.log("ok");
+           types.innerHTML=`<span class="tipo1_pokemon text-base bg-red-500 px-[0.2rem] w-1/4 rounded-sm"> ${data['types'][0]['type']['name']}</span>`
+        }else{
+            types.innerHTML=
+               `<span class="tipo1_pokemon text-base bg-red-500 px-[0.2rem] w-1/4 rounded-sm"> ${data['types'][0]['type']['name']}</span>
+                <span class="tipo2_pokemon text-base bg-red-500 px-[0.2rem] w-1/4 rounded-sm"> ${data['types'][1]['type']['name']}</span>`
+        }
+
+        imagemPokemon.src=data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+    } else {
+        nomePokemon.innerHTML = 'Não encontrado';
+        numeroPokemon.innerHTML = '';
+        imagemPokemon.src='../img/notfound';
+        
+    }
+   
+}
+
+formProcurarPokemon.addEventListener('submit',(event)=>{
+    event.preventDefault();
+
+    renderPokemon(inputProcurarPokemon.value.toLowerCase());
+    
+});
+btnPrevPokemon.addEventListener('click', () => {
+    if (pokemonInicial > 1) {
+        pokemonInicial -= 1;
+      renderPokemon(pokemonInicial);
+    }
+});
+  
+bntNextPokemon.addEventListener('click', () => {
+    pokemonInicial += 1;
+    renderPokemon(pokemonInicial);
+});
+
+
+renderPokemon(pokemonInicial);
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'sombraDivs': ' -3px 4px 0px 0px rgba(212,212,212,0.3) , -5px 7px 0 0px rgba(51,51,51,0.3)',
+        'press': 'inset -4px 4px 0px 0px rgba(51,51,51,0.3)',
+      }
+    },
   },
   plugins: [],
 }
